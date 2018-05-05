2dec19d42ac26f0186d0cfbe7fc316498623be56
gipr1990 <gipr1990@gmail.com>
2018-05-05 17:14:17 -0300

    fgdfg

--- src/main/java/main/DoExamp.java
+++ src/main/java/main/DoExamp.java
@@ -4,29 +4,7 @@ import java.util.Scanner;
 
 public class DoExamp {
 
-    public static void main(String[] args) {
-
-        Scanner entrada = new Scanner(System.in);
-
-        int numInt;
-        int suma = 0;
+    private int edad
 
 
-        do {
-            System.out.println("hola ingresa un numero positivo o -1 para finalizar");
-            numInt = entrada.nextInt();
-
-           if (numInt > 0)
-                    suma += numInt;
-           if (suma >= 100)
-               break;
-        }
-
-        while (numInt != -1);{
-            System.out.println("sumatoria : "+suma);
-
-        }
-
-
-    }
 }
